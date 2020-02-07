650692fde53021c39d5cc2040a51c9fc8f085c99
tavius84 <goldbrass8@gmail.com>
2020-02-07 14:00:17 -0600

    3

---
+++ Chapter 7/src/exercise/BabyNameComparison.java
@@ -0,0 +1,12 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class BabyNameComparison {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name1 = null, name2 = null , name3 = null;
+        String[] names = {name1, name2, name3};
+
+    }
+}
