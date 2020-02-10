81a229048e9bf9e9c5c68ff7bcfa902609e76a4f
tavius84 <goldbrass8@gmail.com>
2020-02-10 13:58:32 -0600

    4

--- Chapter 7/src/exercise/BabyNameComparison.java
+++ Chapter 7/src/exercise/BabyNameComparison.java
@@ -1,12 +1,27 @@
 package exercise;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class BabyNameComparison {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String name1 = null, name2 = null , name3 = null;
-        String[] names = {name1, name2, name3};
+        Random rand = new Random();
+
+        String names[] = new String[3];
+
+        for (int i = 0; i < 3; i++){
+            System.out.println("Enter a baby name >>> ");
+            names[i] = input.nextLine();
+        }
+        for(int i = 0; i < 3; i++) {
+            int r = rand.nextInt(2);
+            StringBuilder str = new StringBuilder(names[r]);
+            str.insert(r, names[i]);
+            System.out.println(str);
+        }
+
+
 
     }
 }
